6b34685ff8f3fc0a3202dea5b22d696b93608572
Cristian Martinez <95260722+Xignys@users.noreply.github.com>
2022-05-22 14:02:14 -0500

    22050202

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,7 @@ const fetchData = async (id) => {
   try {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const data = await res.json();
+
     mostrarCarta(data);
   } catch (error) {
     console.log(error);
@@ -20,17 +21,21 @@ const fetchData = async (id) => {
 const mostrarCarta = (pokemon) => {
   console.log(pokemon);
   const imagen = document.querySelector("#ImagenPokemon");
-  const neimo = document.querySelector("#NombrePokemon").textContent;
-  const id = document.querySelector("#IdPokemon").textContent;
-  const abilites = document.querySelector("#InfoPokemon1").textContent;
+  const neimo = document.querySelector("#NombrePokemon");
+  const id = document.querySelector("#IdPokemon");
+  const ability = document.querySelector("#InfoPokemon1");
+  
 
-  document
-    .querySelector("#aquivaelpoke").setAttribute("src", pokemon.sprites.front_default);
+  document.querySelector("#aquivaelpoke").setAttribute("src", pokemon.sprites.front_default);
   document.querySelector("#aquivaelnombre").textContent = pokemon.name;
   document.querySelector("#aquivaelid").textContent = pokemon.id;
-  document.querySelector("#aquivalainfo1").textContent = pokemon.stat.name;
+  document.querySelector("#aquivalainfo1").textContent = pokemon.abilities[0].ability.name;
+  document.querySelector("#aquivalainfo2").textContent = pokemon.abilities[1].ability.name;
+  document.querySelector("#aquivalainfo3").textContent = pokemon.abilities[2].ability.name;
+  document.querySelector("#aquivalainfo4").textContent = pokemon.abilities[3].ability.name; 
 
   imagen.appendChild(imagen);
   neimo.appendChild(neimo);
   id.appendChild(id);
+  ability.appendChild(ability);
 };
