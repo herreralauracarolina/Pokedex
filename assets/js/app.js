3f1348e3eb2303b875130dd5fc6a22e4026fc24e
Cristian Martinez <95260722+Xignys@users.noreply.github.com>
2022-05-22 15:05:03 -0500

    22051504

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,23 +19,20 @@ const fetchData = async (id) => {
 };
 
 const mostrarCarta = (pokemon) => {
-  console.log(pokemon);
-  const imagen = document.querySelector("#ImagenPokemon");
-  const neimo = document.querySelector("#NombrePokemon");
-  const id = document.querySelector("#IdPokemon");
-  const ability = document.querySelector("#InfoPokemon1");
-  
+
+  console.log(pokemon);  
 
   document.querySelector("#aquivaelpoke").setAttribute("src", pokemon.sprites.front_default);
   document.querySelector("#aquivaelnombre").textContent = pokemon.name;
   document.querySelector("#aquivaelid").textContent = pokemon.id;
+
   document.querySelector("#aquivalainfo1").textContent = pokemon.abilities[0].ability.name;
   document.querySelector("#aquivalainfo2").textContent = pokemon.abilities[1].ability.name;
-  document.querySelector("#aquivalainfo3").textContent = pokemon.abilities[2].ability.name;
-  document.querySelector("#aquivalainfo4").textContent = pokemon.abilities[3].ability.name; 
-
-  imagen.appendChild(imagen);
-  neimo.appendChild(neimo);
-  id.appendChild(id);
-  ability.appendChild(ability);
+  document.querySelector("#aquivalainfo3").textContent = pokemon.abilities[2].ability.name; 
 };
+
+
+// //const imagen = document.querySelector("#ImagenPokemon");
+// const neimo = document.querySelector("#NombrePokemon");
+// const id = document.querySelector("#IdPokemon");
+
