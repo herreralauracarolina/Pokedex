d9ca0a0ee7730de399bf694bddd487b75c5294e8
Cristian Martinez <95260722+Xignys@users.noreply.github.com>
2022-05-23 21:28:07 -0500

    23052128

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,14 +25,20 @@ const mostrarCarta = (pokemon) => {
   document.querySelector("#aquivaelpoke").setAttribute("src", pokemon.sprites.front_default);
   document.querySelector("#aquivaelnombre").textContent = pokemon.name;
   document.querySelector("#aquivaelid").textContent = pokemon.id;
-
-  document.querySelector("#aquivalainfo1").textContent = pokemon.abilities[0].ability.name;
-  document.querySelector("#aquivalainfo2").textContent = pokemon.abilities[1].ability.name;
-  document.querySelector("#aquivalainfo3").textContent = pokemon.abilities[2].ability.name; 
-
+  
   document.querySelector("#aquivaelstat1").textContent = "HP " + pokemon.stats[0].base_stat;
   document.querySelector("#aquivaelstat2").textContent = "ATK " + pokemon.stats[1].base_stat;
   document.querySelector("#aquivaelstat3").textContent = "DEF " + pokemon.stats[2].base_stat;
+  document.querySelector("#aquivalalocation").textContent = "weight: " + pokemon.weight;
+
+  document.querySelector("#aquivaeltype1").textContent = pokemon.types[0].type.name;
+
+  document.querySelector("#aquivalainfo1").textContent = pokemon.abilities[0].ability.name;
+  document.querySelector("#aquivalainfo2").textContent = pokemon.abilities[1].ability.name;
+  document.querySelector("#aquivalainfo3").textContent = pokemon.abilities[2].ability.name;
+  
+  document.querySelector("#aquivaeltype1").textContent = pokemon.types[1].type.name;
+
 
 };
 
