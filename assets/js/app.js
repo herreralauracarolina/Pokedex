e4a3eb106bbf1fe871904bcd345ca4b2e060bce0
Cristian Martinez <95260722+Xignys@users.noreply.github.com>
2022-05-25 13:50:51 -0500

    blankspacesolution2

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,68 +13,72 @@ const fetchData = async (id) => {
     const data = await res.json();
 
     mostrarCarta(data);
-
   } catch (error) {
     console.log(error);
   }
 };
 
 const mostrarCarta = (pokemon) => {
+  console.log(pokemon);
 
-  console.log(pokemon);  
-
-  document.querySelector("#aquivaelpoke").setAttribute("src", pokemon.sprites.front_default); //trae imagen
+  document
+    .querySelector("#aquivaelpoke")
+    .setAttribute("src", pokemon.sprites.front_default); //trae imagen
   document.querySelector("#aquivaelnombre").textContent = pokemon.name; // trae el nombre
   document.querySelector("#aquivaelid").textContent = pokemon.id; // trae el id del pokemon
-  
-  document.querySelector("#aquivaelstat1").textContent = "HP " + pokemon.stats[0].base_stat; //trae el stat de vida
-  document.querySelector("#aquivaelstat2").textContent = "ATK " + pokemon.stats[1].base_stat; //el stat de ataque
-  document.querySelector("#aquivaelstat3").textContent = "DEF " + pokemon.stats[2].base_stat; // el stat de defensa
-  document.querySelector("#aquivalaexp").textContent = "EXP: " + pokemon.base_experience; // la experiencia base
 
-  document.querySelector("#aquivaeltype1").textContent = pokemon.types[0].type.name; // el tipo de pokemon (hierba, veneno, volador, etc etc)
+  document.querySelector("#aquivaelstat1").textContent =
+    "HP " + pokemon.stats[0].base_stat; //trae el stat de vida
+  document.querySelector("#aquivaelstat2").textContent =
+    "ATK " + pokemon.stats[1].base_stat; //el stat de ataque
+  document.querySelector("#aquivaelstat3").textContent =
+    "DEF " + pokemon.stats[2].base_stat; // el stat de defensa
+  document.querySelector("#aquivalaexp").textContent =
+    "EXP: " + pokemon.base_experience; // la experiencia base
 
-  document.querySelector("#aquivalainfo1").textContent = pokemon.abilities[0].ability.name; // trae la habilidad pasiva del pokemon
-  document.querySelector("#aquivalainfo2").textContent = pokemon.abilities[1].ability.name; //trae la segunda habilidad pasiva del pokemon (si tiene)
-  document.querySelector("#aquivalainfo3").textContent = pokemon.abilities[2].ability.name; //trae la tercera habilidad pasiva del pokemon (si tiene)
-  
-  document.querySelector("#aquivaeltype2").textContent = pokemon.types[1].type.name; // algunos pokemon tienen dos tipos (hierba, veneno, etc etc) si tiene la trae
+  document.querySelector("#aquivaeltype1").textContent =
+    pokemon.types[0].type.name; // el tipo de pokemon (hierba, veneno, volador, etc etc)
+
+  document.querySelector("#aquivalainfo1").textContent =
+    pokemon.abilities[0].ability.name; // trae la habilidad pasiva del pokemon
+  document.querySelector("#aquivalainfo2").textContent =
+    pokemon.abilities[1].ability.name; //trae la segunda habilidad pasiva del pokemon (si tiene)
+  document.querySelector("#aquivalainfo3").textContent =
+    pokemon.abilities[2].ability.name; //trae la tercera habilidad pasiva del pokemon (si tiene)
+
+  document.querySelector("#aquivaeltype2").textContent =
+    pokemon.types[1].type.name; // algunos pokemon tienen dos tipos (hierba, veneno, etc etc) si tiene la trae
 };
 
+document.getElementById("abilities").style.display = "block";
+document.getElementById("stats").style.display = "none";
+document.getElementById("types").style.display = "none";
+document.getElementById("expbase").style.display = "none";
+
+const abilities = () => {
   document.getElementById("abilities").style.display = "block";
   document.getElementById("stats").style.display = "none";
   document.getElementById("types").style.display = "none";
   document.getElementById("expbase").style.display = "none";
-
-function abilities(){ 
-
-  document.getElementById("abilities").style.display = "block";
-  document.getElementById("stats").style.display = "none";
-  document.getElementById("types").style.display = "none";
-  document.getElementById("expbase").style.display = "none";
 };
 
-function stats(){ 
-
+const stats = () => {
   document.getElementById("abilities").style.display = "none";
   document.getElementById("stats").style.display = "block";
   document.getElementById("types").style.display = "none";
   document.getElementById("expbase").style.display = "none";
 };
 
-function types(){ 
-
+const types = () => {
   document.getElementById("abilities").style.display = "none";
   document.getElementById("stats").style.display = "none";
   document.getElementById("types").style.display = "block";
   document.getElementById("expbase").style.display = "none";
 };
 
-function expbase(){ 
-
+const expbase = () => {
   document.getElementById("abilities").style.display = "none";
   document.getElementById("stats").style.display = "none";
   document.getElementById("types").style.display = "none";
   document.getElementById("expbase").style.display = "block";
 };
-
